734e9523b747f3a54c5973a55121ae407e79b2e3
IanCG17 <134028152+IanCG17@users.noreply.github.com>
2023-09-18 20:59:09 -0500

    V1

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     PORT: 3002,
     SECRET:'INEXCELSISDEO',
-    MONGODB_URI:'mongodb+srv://albertocpm2002:gazVqKEriDrQkzg9@cluster0.mrbfuak.mongodb.net/'
+    MONGODB_URI:'mongodb+srv://icordova1:pfE6E8oOUpTjCucS@cluster0.sxdeeia.mongodb.net/'
 }
 
 module.exports = config
